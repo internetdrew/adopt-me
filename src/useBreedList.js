0d96761af08ca0eff122caeb5ef582850b80de87
Andrew Rowley <andrew@internetdrew.com>
2022-12-21 14:18:44 -0500

    use useQuery in useBreed hook

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,44 +1,9 @@
-import { useState, useEffect } from 'react';
-
-const localCache = {};
+import { useQuery } from '@tanstack/react-query';
+import fetchBreedList from './fetchBreedList';
 
 const useBreedList = animal => {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState('unloaded');
-
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-      return;
-    }
-
-    if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-      return;
-    }
-
-    requestBreedList();
-
-    async function requestBreedList() {
-      try {
-        setBreedList([]);
-        setStatus('loading');
-
-        const res = await fetch(
-          `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-        );
-        const animalData = await res.json();
-
-        localCache[animal] = animalData.breeds || [];
-        setBreedList(localCache[animal]);
-        setStatus['loaded'];
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  const results = useQuery(['breeds', animal], fetchBreedList);
+  return [results?.data?.breeds ?? [], results.status];
 };
 
 export default useBreedList;
