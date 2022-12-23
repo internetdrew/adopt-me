ac214fd656311e2017c8785d5c852cfd8906ace7
Andrew Rowley <andrew@internetdrew.com>
2022-12-23 09:17:50 -0500

    shorten api response variable name

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -1,15 +1,15 @@
 const fetchSearch = async ({ queryKey }) => {
   const { animal, location, breed } = queryKey[1];
 
-  const apiResponse = await fetch(
+  const apiRes = await fetch(
     `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
   );
 
-  if (!apiResponse.ok) {
+  if (!apiRes.ok) {
     throw new Error(`search response for ${animal} ${location} ${breed}not ok`);
   }
 
-  return apiResponse.json();
+  return apiRes.json();
 };
 
 export default fetchSearch;
