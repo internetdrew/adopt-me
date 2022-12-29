a43306040072141cd8795658682943b8659b205a
Andrew Rowley <andrew@internetdrew.com>
2022-12-29 12:44:35 -0500

    add modal on adopt button click

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import ErrorBoundary from './ErrorBoundary';
 import fetchPet from './fetchPet';
 import Carousel from './Carousel';
 import { capFirstLetter } from './utilities';
+import Modal from './Modal';
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(['details', id], fetchPet);
 
@@ -26,8 +29,19 @@ const Details = () => {
         <h1>{pet.name}</h1>
         <h2>
           {capFirstLetter(pet.animal)} — {pet.breed} — {pet.city}, {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {showModal && (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className='buttons'>
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          )}
         </h2>
       </div>
     </div>
--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 
 class ErrorBoundary extends Component {
   state = { hasError: false };
+
   static getDerivedStateFromError() {
     return { hasError: true };
   }
 
   componentDidCatch(error, info) {
-    console.error('ErrorBoundary component caught an error', error, info);
+    console.error('ErrorBoundary component caught an error:', error, info);
   }
 
   render() {
